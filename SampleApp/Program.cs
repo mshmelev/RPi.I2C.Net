99d9fe657ffeb459b08d22cd8ac294e3b6ac8857
Max Shmelev <mshmelev@gmail.com>
2012-11-28 21:58:45 -0500

    Added read/write example

--- SampleApp/Program.cs
+++ SampleApp/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 
 namespace SampleApp
@@ -11,7 +10,8 @@ namespace SampleApp
 		{
 			using (var bus = RPi.I2C.Net.I2CBus.Open("/dev/i2c-1"))
 			{
-				bus.WriteByte(42, 77);
+				bus.WriteByte(42, 96);
+				byte[] res = bus.ReadBytes(42, 3);
 			}
 		}
 	}
